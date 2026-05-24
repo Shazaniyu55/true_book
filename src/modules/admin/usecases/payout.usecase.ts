ca2575477e753e3ba2e5d0e5ae8526f216bfc79e
shazaniyu55 <shazaniyu@gmail.com>
2026-05-24 16:14:40 +0100

    dashboard

---
+++ src/modules/admin/usecases/payout.usecase.ts
@@ -0,0 +1,44 @@
+import { Injectable } from '@nestjs/common';
+import { EntityManager } from 'typeorm';
+import { Usecase } from '@broker/types';
+import { AdminService } from '../services/admin.service';
+import { AdminListQueryDto } from '../dtos/admin.dto';
+
+@Injectable()
+export class ListPayoutsUsecase extends Usecase {
+  constructor(private readonly adminService: AdminService) {
+    super();
+  }
+
+  async execute(_entityManager: EntityManager, args: AdminListQueryDto) {
+    return this.adminService.listPayouts(args);
+  }
+}
+
+@Injectable()
+export class ApprovePayoutUsecase extends Usecase {
+  constructor(private readonly adminService: AdminService) {
+    super();
+  }
+
+  async execute(
+    _entityManager: EntityManager,
+    args: { id: number; adminEmail: string },
+  ) {
+    return this.adminService.approvePayout(args.id, args.adminEmail);
+  }
+}
+
+@Injectable()
+export class DeclinePayoutUsecase extends Usecase {
+  constructor(private readonly adminService: AdminService) {
+    super();
+  }
+
+  async execute(
+    _entityManager: EntityManager,
+    args: { id: number; reason: string; adminEmail: string },
+  ) {
+    return this.adminService.declinePayout(args.id, args.reason, args.adminEmail);
+  }
+}
