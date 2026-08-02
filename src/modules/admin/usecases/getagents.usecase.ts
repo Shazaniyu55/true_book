c1b57a13b448e7bd9a2de0dfd50dd502058460cc
Papilo <shazaniyu@gmail.com>
2026-08-02 05:28:14 +0100

    'server'

--- src/modules/admin/usecases/getagents.usecase.ts
+++ src/modules/admin/usecases/getagents.usecase.ts
@@ -10,7 +10,29 @@ export class GetAgentsUsecase extends Usecase {
     super();
   }
 
-  async execute(_entityManager: EntityManager, _args: any, args: { query?: AdminListQueryDto }) {
+  async execute(
+    _entityManager: EntityManager,
+    args: { query?: AdminListQueryDto },
+    _args?: any,
+  ) {
     return this.adminService.getAgents(args?.query ?? {});
   }
 }
+// import { Injectable } from '@nestjs/common';
+// import { EntityManager } from 'typeorm';
+// import { Usecase } from '@broker/types';
+// import { AdminService } from '../services/admin.service';
+// import { AdminListQueryDto } from '../dtos/admin.dto';
+
+// @Injectable()
+// export class GetAgentsUsecase extends Usecase {
+//   constructor(private readonly adminService: AdminService) {
+//     super();
+//   }
+
+//   async execute(_entityManager: EntityManager, _args: any, args: { query?: AdminListQueryDto }) {
+//       console.log('usecase _args:', _args);
+//   console.log('usecase args:', args);
+//     return this.adminService.getAgents(args?.query ?? {});
+//   }
+// }
