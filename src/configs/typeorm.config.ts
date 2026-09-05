762c064594143a3f1eeae198ca380e793cc976f9
Papilo <shazaniyu@gmail.com>
2026-09-05 14:11:45 +0100

    server1

--- src/configs/typeorm.config.ts
+++ src/configs/typeorm.config.ts
@@ -11,6 +11,7 @@ export const dataSource = {
   type: 'postgres',
   host: process.env.DB_HOST,
   port: parseInt(process.env.DB_PORT ?? '5432', 10),
+  url: process.env.DATABASE_URL,
   username: process.env.DB_USER,
   password: process.env.DB_PASS,
   database: process.env.DB_NAME,
