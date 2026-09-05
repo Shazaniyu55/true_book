56fe7e1a17af9dbdb811cc46b3b6a785c87fdda1
Papilo <shazaniyu@gmail.com>
2026-09-05 13:57:43 +0100

    server1

--- src/configs/typeorm.config.ts
+++ src/configs/typeorm.config.ts
@@ -5,18 +5,17 @@ import 'reflect-metadata';
 
 import { config as dotenvConfig } from 'dotenv';
 
-// We don't have access to the @nestjs/config module when running the
-// migrations, so we need to load the environment variables manually.
 dotenvConfig();
+
 export const dataSource = {
-  database: process.env.DB_NAME,
+  type: 'postgres',
   host: process.env.DB_HOST,
-  port: parseInt(process.env.DB_PORT, 10),
+  port: parseInt(process.env.DB_PORT ?? '5432', 10),
   username: process.env.DB_USER,
   password: process.env.DB_PASS,
+  database: process.env.DB_NAME,
   autoLoadEntities: true,
   synchronize: false,
-  type: 'postgres',
   logging: process.env.DATABASE_LOGGING === 'true',
   migrationsTransactionMode: 'each',
   entities: ['dist/**/*.entity.{js,ts}'],
@@ -24,13 +23,47 @@ export const dataSource = {
   dropSchema: process.env.NODE_ENV === 'test',
   migrationsTableName: 'migrations',
   migrations: ['dist/migrations/**/*{.ts,.js}'],
-  retryAttempts: parseInt(process.env.DATABASE_RETRY_ATTEMPTS),
-  //ssl: process.env.NODE_ENV === 'production' ? { rejectUnauthorized: false } : false, // ← changed
-  ssl: {
-  rejectUnauthorized: false,
-},
+  retryAttempts: parseInt(process.env.DATABASE_RETRY_ATTEMPTS ?? '10', 10),
+  ssl: process.env.DB_SSL === 'true' ? { rejectUnauthorized: false } : false,
 };
 
 export default registerAs('typeorm', () => dataSource);
 
 export const connectionSource = new DataSource(dataSource as DataSourceOptions);
+
+// import { registerAs } from '@nestjs/config';
+// import { DataSource, DataSourceOptions } from 'typeorm';
+
+// import 'reflect-metadata';
+
+// import { config as dotenvConfig } from 'dotenv';
+
+// // We don't have access to the @nestjs/config module when running the
+// // migrations, so we need to load the environment variables manually.
+// dotenvConfig();
+// export const dataSource = {
+//   database: process.env.DB_NAME,
+//   host: process.env.DB_HOST,
+//   port: parseInt(process.env.DB_PORT, 10),
+//   username: process.env.DB_USER,
+//   password: process.env.DB_PASS,
+//   autoLoadEntities: true,
+//   synchronize: false,
+//   type: 'postgres',
+//   logging: process.env.DATABASE_LOGGING === 'true',
+//   migrationsTransactionMode: 'each',
+//   entities: ['dist/**/*.entity.{js,ts}'],
+//   migrationsRun: process.env.NODE_ENV === 'test',
+//   dropSchema: process.env.NODE_ENV === 'test',
+//   migrationsTableName: 'migrations',
+//   migrations: ['dist/migrations/**/*{.ts,.js}'],
+//   retryAttempts: parseInt(process.env.DATABASE_RETRY_ATTEMPTS),
+//   //ssl: process.env.NODE_ENV === 'production' ? { rejectUnauthorized: false } : false, // ← changed
+//   ssl: {
+//   rejectUnauthorized: false,
+// },
+// };
+
+// export default registerAs('typeorm', () => dataSource);
+
+// export const connectionSource = new DataSource(dataSource as DataSourceOptions);
